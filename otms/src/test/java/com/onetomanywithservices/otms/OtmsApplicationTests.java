5d65481ed04a77ef5df5fc0dee09afb678c497d3
CindoddCindy <cindodcindy@gmail.com>
2021-02-07 15:42:58 +0700

    tambah kelas kelas

---
+++ otms/src/test/java/com/onetomanywithservices/otms/OtmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onetomanywithservices.otms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
